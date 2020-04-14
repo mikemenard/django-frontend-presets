0f36890287106c1c7ab7bc92bdc6edc8efe1e6a6
Michael Menard <hello@mikemenard.com>
2020-04-13 22:20:04 -0400

    repackage app

---
+++ docs/installation.rst
@@ -0,0 +1,12 @@
+============
+Installation
+============
+
+At the command line::
+
+    $ easy_install django-frontend-presets
+
+Or, if you have virtualenvwrapper installed::
+
+    $ mkvirtualenv django-frontend-presets
+    $ pip install django-frontend-presets
---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-frontend-presets
+------------
+
+Tests for `django-frontend-presets` models module.
+"""
+
+from django.test import TestCase
+
+from django_frontend_presets import models
+
+
+class TestDjango_frontend_presets(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
